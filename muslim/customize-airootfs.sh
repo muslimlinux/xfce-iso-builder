0c968ce9a578116578c69eeb7e7462a1d44e1a9a
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-01-19 09:25:20 +0300

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -15,32 +15,37 @@ wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.g
 apt update
 apt install librewolf -y
 
-## Firefox latest User Installer
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/firefox_user_installer_0.1.0_amd64.deb
+### desktop-base package
+wget https://github.com/muslimos/desktop-base/raw/main/desktop-base-noupdate.deb
+
+### pardus lightdm greeter
+wget https://github.com/muslimos/pardus-lightdm-greeter/releases/download/current/pardus-lightdm-greeter_0.0.1_all.deb
 
 ### 17g installer
 wget https://github.com/muslimos/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 
-### AppImage launcher and Pardus package installer
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/appimagelauncher_2.2.0.deb
-wget http://depo.pardus.org.tr/pardus/pool/main/p/pardus-package-installer/pardus-package-installer_0.5.0~Beta2_all.deb
+## Firefox latest User Installer
+wget https://github.com/muslimos/debhane/raw/main/deb/firefox_user_installer_0.1.0_amd64.deb
 
-### Pardus lightdm greeter
-wget https://github.com/muslimos/pardus-lightdm-greeter/releases/download/current/pardus-lightdm-greeter_0.0.1_all.deb
+### AppImage launcher
+wget https://github.com/muslimos/debhane/raw/main/deb/appimagelauncher_2.2.0.deb
 
 ## QMPlay2 Video and Sound Player
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/qmplay2_22.10.23-1~par21_amd64.deb
+wget https://github.com/muslimos/debhane/raw/main/deb/qmplay2_22.10.23-1~par21_amd64.deb
 
 ## Webapp Manager
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/webapp-manager_1.2.8_all.deb
+wget https://github.com/muslimos/debhane/raw/main/webapp-manager_1.2.8_all.deb
 
 ## Foliate reader
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/foliate_2.6.4_all.deb
-
-### desktop-base
-wget https://github.com/muslimos/desktop-base/raw/main/desktop-base-noupdate.deb
+wget https://github.com/muslimos/debhane/raw/main/deb/foliate_2.6.4_all.deb
 
 ## Papirus-teal-icons
-wget https://github.com/mobilturka/mt-repo/raw/main/deb/papirus-teal-icon-theme.deb
+wget https://github.com/muslimos/debhane/raw/main/deb/papirus-teal-icon-theme.deb
+
+### System Monitoring Center
+wget https://github.com/muslimos/debhane/raw/main/system-monitoring-center_1.39.1_all.deb
+
+### Pardus package installer
+wget http://depo.pardus.org.tr/pardus/pool/main/p/pardus-package-installer/pardus-package-installer_0.5.0~Beta2_all.deb
 
 apt install ./*.deb -yq --allow-downgrades
