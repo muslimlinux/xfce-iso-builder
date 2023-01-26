bc4af4e361da00e86b5de2f24a93e80313dd6848
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-01-26 23:29:18 +0300

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -30,7 +30,7 @@ wget https://github.com/muslimos/debhane/raw/main/foliate_2.6.4_all.deb
 
 ## Papirus-teal-icons
 wget https://github.com/muslimos/debhane/raw/main/papirus-teal-icon-theme.deb
-wget https://github.com/muslimos/desktop-base/raw/main/desktop-base-noupdate.deb
+wget https://github.com/muslimos/desktop-base/raw/main/desktop-base-end.deb
 #wget https://github.com/muslimos/debhane/raw/main/muslim-grub-theme.deb
 wget https://github.com/muslimos/debhane/raw/main/pardus-lightdm-greeter_0.0.3_all.deb
 
