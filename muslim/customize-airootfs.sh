0ea1184c28fe0b7193f2238eb9399dde448efaa5
muslimlinux <123402985+muslimlinux@users.noreply.github.com>
2023-01-31 14:56:31 +0300

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -15,7 +15,7 @@ wget https://github.com/muslimos/debhane/raw/main/foliate_2.6.4_all.deb
 wget https://github.com/muslimos/debhane/raw/main/papirus-teal-icon-theme.deb
 wget https://github.com/muslimos/debhane/raw/main/muslim-grub-theme.deb
 wget https://github.com/muslimos/debhane/raw/main/pardus-lightdm-greeter_0.0.3_all.deb
-wget https://github.com/muslimos/debhane/raw/main/system-monitoring-center_1.39.1_all.deb
+wget https://github.com/muslimos/debhane/raw/main/system-monitoring-center_1.40.0_all.deb
 wget https://github.com/muslimos/debhane/raw/main/pardus-package-installer_0.5.0~Beta2_all.deb
 
 apt install ./*.deb -yq --allow-downgrades
